e9a9e819545da1bd0f71f0eee290489b2b57a900
ilwapoi <maslolpavl@gmail.com>
2019-09-18 20:25:37 +0300

    start use PYRAMID add ping route

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
 # Welcome to Best Desition System Making
 # this is the main file which run the application
-# this application will based on Flask by background
+# this application will based on pyramid by background
+from wsgiref.simple_server import make_server
+from pyramid.config import Configurator
+from pyramid.response import Response
+
+def ping_route(request):
+    return Response('pong')
+
+if __name__ == '__main__':
+    with Configurator() as config:
+        config.add_route('ping', '/ping')
+        config.add_view(ping_route, route_name='ping')
+        app = config.make_wsgi_app()
+    server = make_server('0.0.0.0', 6543, app)
+    server.serve_forever()
